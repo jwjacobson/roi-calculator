77bf53d4f7706b744d2f99732434aa7f35fefb6a
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2023-03-04 18:21:22 -0500

    Finish program mechanics

--- roi_calculator.py
+++ roi_calculator.py
@@ -1,27 +1,27 @@
 #!/usr/bin/python
 
-# welcome
-# enter price
-# calculate income
-# enter expenses
-# calculate cashflow
-# enter dp/closing_costs
-# calculate roi
-
-class Property:
+class Property():
     
     def __init__(self, price):
         self.price = price
+        self.monthly_income = int
+        self.monthly_expenses = int
+        self.cash_flow = int
+        self.initial_investment = int
+        self.roi = float
+
 
     def get_monthly_income(self):
-        return self.price * .05
+        self.monthly_income = int(self.price * .05) 
+        return self.monthly_income
+        
 
     def get_expenses(self):
-        monthly_expenses = 0
-        possible_expenses = {'tax', 'insurance', 'utilities', 'HOA', 'lawn and snow', 'vacancy', 'repairs', 'capital expenditures', 'property manager', 'mortgage'}
-        print("\nNext we'll calculate your monthly expenses.")
+        self.monthly_expenses = 0
+        possible_expenses = {'Tax', 'Insurance', 'Utilities', 'HOA', 'Lawn and snow', 'Vacancy', 'Repairs', 'Capital expenditures', 'Property manager', 'Mortgage'}
+        print("\nNext we'll calculate your monthly expenses.\n")
         for item in possible_expenses:
-            is_there = input(f"{item.title()}? (y/n) ")
+            is_there = input(f"{item}? (y/n) ")
             while is_there.lower() != 'y' and is_there.lower() != 'n':
                 is_there = input("Answer [Y]es or [N]o. ")
             if is_there.lower() == 'n':
@@ -30,31 +30,36 @@ class Property:
                 expense = input("How much? ")
                 while expense.isdigit() == False:
                     expense = input("Price must be an integer. Try again. ")
-                monthly_expenses += int(expense)
-        return monthly_expenses
+                self.monthly_expenses += int(expense)
+        return self.monthly_expenses
     
     def get_cash_flow(self):
         print("\nBased on your monthly income and expenses, we can calculate your cash flow.")
-        return monthly_income - monthly_expenses
+        self.cash_flow = self.monthly_income - self.monthly_expenses 
+        return self.cash_flow
 
     def get_investment(self):
-        pass
+        self.initial_investment = 0
+        possible_investments = ["Down payment", "Closing costs", "Rehab", "Anything else"]
+        print("\nNow we need to calculate your initial investment.\n")
+        for item in possible_investments:
+            is_there = input(f"{item}? (y/n) ")
+            while is_there.lower() != 'y' and is_there.lower() != 'n':
+                is_there = input("Answer [Y]es or [N]o. ")
+            if is_there.lower() == 'n':
+                continue
+            else:
+                investment = input("How much? ")
+                while investment.isdigit() == False:
+                    investment = input("Price must be an integer. Try again. ")
+                self.initial_investment += int(investment)
+        return self.initial_investment
 
     def get_roi(self):
-        pass
-
-#CASHFLOW
-# cashflow = total_income - total_expenses
-
-#CASHONCASH ROI
-# down_payment = 40000 # 20%
-# closing_costs = 3000
-# rehab = 7000
-# misc = 0
-
-# total_investment = 50000
-
-# roi = annual_cash_flow / total_investment
+        print("Now we can calculate your cash on cash return.")
+        print("We find it by dividing your annual cash flow by your initial investment.")
+        self.roi = ((self.cash_flow * 12) / self.initial_investment) * 100
+        return self.roi
 
 def run_calc():
     print("* * * Rental Property ROI Calculator (RPROIC) * * *")
@@ -62,11 +67,10 @@ def run_calc():
     while price.isdigit() == False:
         price = input("Price must be an integer. Try again. ")
     property = Property(int(price))
-    monthly_income = property.get_monthly_income()
-    print(f"Based on the price of the property, you can expect a monthly income of {monthly_income}.")
-    property.get_expenses()
-    print(f"Your monthly expenses are {monthly_expenses}.")
-    property.get_cash_flow()
-
+    print(f"\nBased on the price of the property, you can expect a monthly income of {property.get_monthly_income()}.")
+    print(f"\nYour monthly expenses are {property.get_expenses()}.")
+    print(f"\nYour monthly cashflow is {property.get_cash_flow()}.")
+    print(f"\nYour initial investment is {property.get_investment()}.")
+    print(f"\nYour cash on cash return is {property.get_roi()} percent.")
 
 run_calc()
