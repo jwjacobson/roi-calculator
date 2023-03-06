c6e4c08619b8c3b752c35de64e29cdb3d9f717ac
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2023-03-06 00:43:54 -0500

    Finish colors, adjust formatting

--- colors.py
+++ colors.py
@@ -1,14 +1,1 @@
-# source: https://stackoverflow.com/questions/25709798/color-a-strings-segments-in-python
-import termcolor
-
-def colorChanger(s, firstSeg, secondSeg, thirdSeg, fourthSeg, hyphen):
-     colors = [firstSeg, secondSeg, thirdSeg, fourthSeg]
-     frags = s.split('-')
-     for ind, color in enumerate(colors):
-         frags[ind] = frags[ind].replace(frags[ind], termcolor.colored(frags[ind], color))
-     return termcolor.colored('-', hyphen).join(frags)
- 
-string = "type-name-function-location"
-
-print(colorChanger(string, 'red', 'green', 'yellow', 'magenta', 'cyan'))
-print(colorChanger(string, 'yellow', 'blue', 'cyan', 'red', 'grey'))
+print("\u001b[44;1mHello")
--- roi_calculator.py
+++ roi_calculator.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-from termcolor import cprint
+from prettytable.colortable import ColorTable, Themes
 
 class Property():
     
@@ -11,80 +11,105 @@ class Property():
         self.initial_investment = int
         self.roi = float
 
-
     def get_monthly_income(self):
         self.monthly_income = int(self.price * .05) 
         return self.monthly_income
-        
 
     def get_expenses(self):
         self.monthly_expenses = 0
         possible_expenses = {'Tax', 'Insurance', 'Utilities', 'HOA', 'Lawn and snow', 'Vacancy', 'Repairs', 'Capital expenditures', 'Property manager', 'Mortgage'}
-        print("\nNext we'll calculate your monthly expenses.\n")
+        print("\n")
+        print("\u001b[44;1m~\u001b[0m"*84)
+        print("\n\u001b[36mNext we'll calculate your monthly expenses.\n")
         for item in possible_expenses:
-            is_there = input(f"{item}? (y/n) ")
+            is_there = input(f"\u001b[33m{item}? \u001b[36m(y/n) ")
             while is_there.lower() != 'y' and is_there.lower() != 'n':
                 is_there = input("Answer [Y]es or [N]o. ")
             if is_there.lower() == 'n':
                 continue
             else:
-                expense = input("How much? ")
+                expense = input("\u001b[33mHow much?\u001b[36m ")
                 while expense.isdigit() == False or int(expense) < 1:
-                    cprint("\nValue must be a positive integer. Try again.", "red", attrs=["bold"] )
-                    expense = input("\nHow much? ")
+                    print("\u001b[31;1mValue must be a positive integer. Try again.\u001b[0m")
+                    expense = input("\n\u001b[33mHow much?\u001b[36m ")
                 self.monthly_expenses += int(expense)
         return self.monthly_expenses
     
     def get_cash_flow(self):
-        print("\nBased on your monthly income and expenses, we can calculate your cash flow.")
+        print("\n")
+        print("\u001b[44;1m~\u001b[0m"*84)
+        print("\n\u001b[36mBased on your monthly income and expenses, we can calculate your cash flow.")
         self.cash_flow = self.monthly_income - self.monthly_expenses 
         return self.cash_flow
 
     def get_investment(self):
         self.initial_investment = 0
-        possible_investments = ["Down payment", "Closing costs", "Rehab", "Anything else"]
-        print("\nNow we need to calculate your initial investment.\n")
+        possible_investments = ["Down payment", "Closing costs", "Rehabilitation", "Anything else"]
+        print("\n")
+        print("\u001b[44;1m~\u001b[0m"*84)
+        print("\n\u001b[36mNow we need to calculate your initial investment.\n")
         for item in possible_investments:
-            is_there = input(f"{item}? (y/n) ")
+            is_there = input(f"\u001b[33m{item}? \u001b[36m(y/n) ")
             while is_there.lower() != 'y' and is_there.lower() != 'n':
                 is_there = input("Answer [Y]es or [N]o. ")
             if is_there.lower() == 'n':
                 continue
             else:
-                investment = input("How much? ")
+                investment = input("\u001b[33mHow much?\u001b[36m ")
                 while investment.isdigit() == False or int(investment) < 1:
-                    cprint("\nValue must be a positive integer. Try again.", "red", attrs=["bold"] )
-                    investment = input("\nHow much? ")
+                    print("\u001b[31;1mValue must be a positive integer. Try again.\u001b[0m")
+                    investment = input("\n\u001b[33mHow much?\u001b[36m ")
                 self.initial_investment += int(investment)
         while self.initial_investment == 0:
-                cprint("\nInitial investment cannot be 0 (produces infinite return)", "red", attrs=["bold"])
-                self.initial_investment = input("\nHow much did you really invest? ")
+                print("\u001b[31;1mInitial investment cannot be 0 (produces infinite return)\u001b[0m")
+                self.initial_investment = input("\n\u001b[33mHow much did you really invest?\u001b[36m ")
                 while self.initial_investment.isdigit() == False or int(self.initial_investment) < 1:
-                    cprint("\nValue must be a positive integer. Try again.", "red", attrs=["bold"] )
-                    self.initial_investment = input("\nHow much did you really invest? ")
+                    print("\u001b[31;1mValue must be a positive integer. Try again.\u001b[0m")
+                    self.initial_investment = input("\n\u001b[33mHow much did you really invest?\u001b[36m ")
                 self.initial_investment = int(self.initial_investment)
         return self.initial_investment
 
     def get_roi(self):
-        print("Now we can calculate your cash on cash return.")
+        print("\n")
+        print("\u001b[44;1m~\u001b[0m"*84)
+        print("\n\u001b[36mNow we can calculate your cash on cash return.")
         print("We find it by dividing your annual cash flow by your initial investment.")
         self.roi = ((self.cash_flow * 12) / self.initial_investment) * 100
         return self.roi
 
+    def get_summary(self):
+        print("\nPreparing your summary...")
+        entries = [
+            ["Market Value", "$ " + str(self.price)],
+            ["Monthly Income", "$ " + str(self.monthly_income)],
+            ["Monthley Expenses", "$ " + str(self.monthly_expenses)],
+            ["Monthly Cashflow", "$ " + str(self.cash_flow)],
+            ["Initial Investment", "$ " + str(self.initial_investment)],
+            ["Rate of Return", str(self.roi) + "%"]
+            ]
+        x = ColorTable(theme=Themes.OCEAN)
+        x.field_names = ["Category", "Value"]
+        x.add_rows(entries)
+        # x.align["Category"] = "l"
+        x.align["Value"] = "l"
+        print(x)
+
 def run_calc():
-    cprint("#"*42, "light_cyan")
-    cprint("# * * Rental Property ROI Calculator * * #", "magenta", attrs=["bold"])
-    cprint("#"*42, "light_cyan")
-    price = input("\nWhat is the market value of the property? ")
+    print("\n")
+    print("\u001b[44;1m#\u001b[0m"*42)
+    print("\u001b[44;1m# * * Rental Property ROI Calculator * * #\u001b[0m")
+    print("\u001b[44;1m#\u001b[0m"*42)
+    price = input("\n\u001b[36mWhat is the market value of the property? (in USD) ")
     while price.isdigit() == False or int(price) < 1:
-        cprint("\nValue must be a positive integer. Try again.", "red", attrs=["bold"] )
-        price = input("\nWhat is the market value of the property? ")
+        print("\u001b[31;1mValue must be a positive integer. Try again.\u001b[0m")
+        price = input("\n\u001b[36mWhat is the market value of the property? ")
     property = Property(int(price))
-    print(f"\nBased on the the property's market value, you can expect a monthly income of {property.get_monthly_income()}.")
-    print(f"\nYour monthly expenses are {property.get_expenses()}.")
-    print(f"\nYour monthly cashflow is {property.get_cash_flow()}.")
-    print(f"\nYour initial investment is {property.get_investment()}.")
-    print(f"\nYour cash on cash return is {property.get_roi()} percent.")
+    print(f"\nBased on the the property's market value, you can expect a \u001b[33;1mmonthly income\u001b[0m \u001b[36mof \u001b[33;1m${property.get_monthly_income()}\u001b[0m\u001b[34m.")
+    print(f"\nYour \u001b[33;1mmonthly expenses\u001b[0m \u001b[36mare \u001b[33;1m${property.get_expenses()}\u001b[36m.")
+    print(f"\nYour \u001b[33;1mmonthly cashflow\u001b[0m\u001b[36m is \u001b[33;1m${property.get_cash_flow()}\u001b[36m.")
+    print(f"\nYour \u001b[33;1minitial investment\u001b[0m\u001b[36m is \u001b[33;1m${property.get_investment()}\u001b[36m.")
+    print(f"\nYour \u001b[33;1mcash on cash return\u001b[0m\u001b[36m is \u001b[33;1m{property.get_roi()}%\u001b[0m\u001b[36m.")
+    property.get_summary()
 
 run_calc()
 
